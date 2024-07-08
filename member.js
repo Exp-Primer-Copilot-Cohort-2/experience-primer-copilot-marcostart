24ef99e5df2334d7f3d291db21784b8ba3f8fc2a
Marcolin <110327931+marcostart@users.noreply.github.com>
2024-07-08 11:40:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMember() {
+    var skills = {
+        skills: {
+            "HTML": "90%",
+            "CSS": "80%",
+            "Javascript": "70%",
+            "React": "60%",
+            "Node": "50%",
+            "MongoDB": "40%"
+        }
+    }
+    return skills;
+}
